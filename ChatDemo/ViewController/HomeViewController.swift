408b2c2be079c6e92e188682212a05b725363136
Will_Weng <Will_Weng@cmoney.com.tw>
2023-02-22 11:24:28 +0800

    feat: 基礎UI 建設

--- ChatDemo/ViewController/HomeViewController.swift
+++ ChatDemo/ViewController/HomeViewController.swift
@@ -15,6 +15,13 @@ class HomeViewController: UIViewController {
         return vm
     }()
 
+    private lazy var displayTableView: UITableView = {
+        var tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+
     // MARK: - 生命週期
 
     override func viewDidLoad() {
@@ -33,9 +40,35 @@ class HomeViewController: UIViewController {
 
     private func setupUI() {
         view.backgroundColor = .systemRed
+
+        view.addSubview(displayTableView)
+        displayTableView.snp.makeConstraints { make in
+            make.top.bottom.leading.trailing.equalToSuperview()
+        }
+        displayTableView.register(HomeTableViewCell.self, forCellReuseIdentifier: "\(HomeTableViewCell.self)")
     }
 
     private func getNetworkItem() {
         viewModel.getNetworkItem()
     }
+}
+
+// MARK: - tableViewDelegate
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = displayTableView.dequeueReusableCell(withIdentifier: "\(HomeTableViewCell.self)", for: indexPath) as? HomeTableViewCell else {
+            print("will - get cell error")
+            return UITableViewCell()
+        }
+
+        return cell
+    }
+
+
 }
