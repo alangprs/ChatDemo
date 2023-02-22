1e541c680ebf37877d49752a515aab4b31abe2a4
Will_Weng <Will_Weng@cmoney.com.tw>
2023-02-22 10:18:18 +0800

    feat: 開 homeVC 檔案

--- ChatDemo/ViewController/HomeViewController.swift
+++ ChatDemo/ViewController/HomeViewController.swift
@@ -1,19 +1,18 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  ChatDemo
 //
 //  Created by cm0768 on 2023/2/22.
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemRed
     }
-
-
 }
-
